43808e980d5a3945b4b3dd98ccd702bfbc79a40b
HU <if21304029@ga.ttc.ac.jp>
2023-02-13 16:15:51 +0900

    cm

---
+++ src/jp/ac/ttc/webapp/HelloServlet.java
@@ -0,0 +1,26 @@
+package jp.ac.ttc.webapp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloServlet extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.write("<h1>hello, world</h1>");
+	}
+
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
